1a6ab7687156583a8b282e5a19cd1871745dcd56
YanZhiwei <Yan.Zhiwei@outlook.com>
2024-06-22 21:51:45 +0800

    增加从locator文件加载显示

--- Recorder/ViewModels/MainWindowViewModel.cs
+++ Recorder/ViewModels/MainWindowViewModel.cs
@@ -1,32 +1,51 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
 using Mortise.Accessibility.Locator.Abstractions;
+using Mortise.UiaAccessibility;
 using Recorder.Models;
 using Tenon.Serialization.Abstractions;
 
 namespace Recorder.ViewModels;
 
-public class MainWindowViewModel(ISerializer serializer, IAccessibleLocatorStorage accessibleLocatorStorage)
+public class MainWindowViewModel
     : ViewModelBase
 {
-    private readonly IAccessibleLocatorStorage _accessibleLocatorStorage = accessibleLocatorStorage;
-    private readonly ISerializer _serializer = serializer;
+    private readonly IAccessibleLocatorStorage _accessibleLocatorStorage;
+    private readonly string _appData;
+    private readonly ISerializer _serializer;
 
-    public ObservableCollection<Node> Nodes { get; } =
-    [
-        new Node("Animals", new ObservableCollection<Node>
-        {
-            new("Mammals", new ObservableCollection<Node>
-            {
-                new("Lion"), new("Cat"), new("Zebra")
-            })
-        }),
+    public MainWindowViewModel()
+    {
 
-        new Node("Animals", new ObservableCollection<Node>
+    }
+    public MainWindowViewModel(ISerializer serializer, IAccessibleLocatorStorage accessibleLocatorStorage)
+    {
+        _serializer = serializer;
+        _accessibleLocatorStorage = accessibleLocatorStorage;
+        _appData = Path.Combine(AppContext.BaseDirectory, "locators");
+        Nodes = Initializelocators();
+    }
+
+    public ObservableCollection<Node> Nodes { get; } 
+
+    private ObservableCollection<Node> Initializelocators()
+    {
+        var nodes = new ObservableCollection<Node>();
+        if (!Directory.Exists(_appData)) return nodes;
+        var locatorFiles = Directory.EnumerateFiles(_appData, "*.locator").ToArray();
+        foreach (var locatorFile in locatorFiles)
         {
-            new("Mammals", new ObservableCollection<Node>
-            {
-                new("Lion"), new("Cat"), new("Zebra")
-            })
-        })
-    ];
+            var locatorName = Path.GetFileNameWithoutExtension(locatorFile);
+            var locatorJsonString = File.ReadAllText(locatorFile, Encoding.UTF8);
+            var accessibles = _serializer.DeserializeObject<UiaAccessible[]>(locatorJsonString);
+            var locator = new Node(locatorName, new ObservableCollection<Node>());
+            foreach (var accessible in accessibles) locator.SubNodes.Add(new Node(accessible.UniqueId));
+            nodes.Add(locator);
+        }
+
+        return nodes;
+    }
 }
